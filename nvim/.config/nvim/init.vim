61964b4e752cb77f75cbaa409d3a960af6b38037
c50a326 <c50a326@tutanota.com>
2018-04-30 13:19:30 +0545

    Initial commit.

---
+++ nvim/.config/nvim/init.vim
@@ -0,0 +1,6 @@
+call plug#begin('~/.local/share/nvim/plugged')
+Plug 'tpope/vim-sensible'
+Plug 'junegunn/seoul256.vim'
+call plug#end()
+
+colo seoul256
---
+++ remember.sh
@@ -0,0 +1,2 @@
+   curl -fLo ~/.dotfiles/vim/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+   curl -fLo ~/.dotfiles/nvim/.local/share/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
