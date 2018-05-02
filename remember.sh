800242d546c73fa998796087559a3b85651ca499
c50a326 <c50a326@tutanota.com>
2018-05-02 09:29:12 +0545

    Moved notes into handbook repo.

--- remember.sh
+++
@@ -1,6 +0,0 @@
-curl -fLo ~/.dotfiles/vim/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-curl -fLo ~/.dotfiles/nvim/.local/share/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-
-# http://vim.wikia.com/wiki/Avoid_the_escape_key
-# norvig 21 days
-# awesome awesome
